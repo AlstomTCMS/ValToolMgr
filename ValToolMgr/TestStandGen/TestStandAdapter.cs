9a15cdf8a0a9915f98da0c378b5fdfeff25ae5b0
gmanciet <guillaume.manciet@alstom.com>
2013-04-02 15:51:07 +0400

    Correction for issue #57.

--- ValToolMgr/TestStandGen/TestStandAdapter.cs
+++ ValToolMgr/TestStandGen/TestStandAdapter.cs
@@ -3,14 +3,14 @@ using System.Text.RegularExpressions;
 
 namespace TestStandGen
 {
-    class TestStandAdapter
+    public class TestStandAdapter
     {
         public static string protectText(string value)
         {
             value = value.Replace("\\\\", "\\");
             value = value.Replace("\\", "\\\\");
             string output = Regex.Replace(value, "(\r\n|\n)", "\\n");
-            value = value.Replace("\"", "\\\"");
+            value = value.Replace("\"", "\\\\\\\"");
             return value;
         }
     }
